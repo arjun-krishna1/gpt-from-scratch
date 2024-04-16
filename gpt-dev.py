0149dbd0b8ac6f64aca3a71b269fd213970afb7f
Arjun Krishna <arjunkrishna1306@gmail.com>
2024-04-16 09:46:56 -0400

    get vocab of possible tokens

--- gpt-dev.py
+++ gpt-dev.py
@@ -4,3 +4,14 @@ with open('input.txt', 'r', encoding='utf-8') as f:
     text = f.read()
 
 print("length of dataset in characters: ", len(text))
+
+print(text[:1000])
+
+# get the set of all the characters that occur in text set(text)
+# turn that into an arbitrary ordering list()
+# sort the ordering sorted
+chars = sorted(list(set(text)))
+vocab_size = len(chars)
+print(''.join(chars))
+print(vocab_size)
+# possible elements characters the model can see or output
