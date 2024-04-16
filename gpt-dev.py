81dea33ce8f337b01bef06a84f0cb61ac551908a
Arjun Krishna <arjunkrishna1306@gmail.com>
2024-04-16 12:55:14 -0400

    add pytorch

--- gpt-dev.py
+++ gpt-dev.py
@@ -15,3 +15,40 @@ vocab_size = len(chars)
 print(''.join(chars))
 print(vocab_size)
 # possible elements characters the model can see or output
+
+# TOKENIZE: convert the raw text as a string
+# into some sequence of integers
+# according to some vocabulary of possibl elements
+# We will just make a character level language model
+# translate individual characters into integers
+
+# create a mapping from characters to integers
+char_to_int_map = { ch:i for i,ch in enumerate(chars) }
+int_to_char_map = { i:ch for i,ch in enumerate(chars) }
+# encoder: take a string, output a list of integers
+encode = lambda s:[char_to_int_map[c] for c in s]
+# decoder: take a list of integers, output a string
+decode = lambda l: ''.join([int_to_char_map[i] for i in l])
+
+print(encode("HELLO WORLD ARJUN HERE!"))
+print(decode(encode("HELLO WORLD ARJUN HERE!")))
+
+# TOKENIZATION: translation to integers and back for an arbitrary string
+# done on a character level, usually done on more complex tokens
+# many other schemas for tokenizaTION (RATHER THAN CHAR LEVEL)
+# google sentencepiece
+# subwords -> not encoding entire words, but also not individual characters
+# tiktoken -> tokenizer, has 50,000 tokens (instead of 65 for our character level based)
+# trade-off codebook size, sequence lengths
+# the more tokens in vocab, the smaller the integer sequence translation of a string is
+# TOKENIZATION: a way to break up any arbitrary string into a list of integers (recognized tokens in vocab)
+
+# encode the entire text dataset and store it into a pytorch Tensor
+import torch
+# take all of the text in tiny shakespeare, encode it
+# and then wrap it in torch.tensor
+# to get the data tensor
+data = torch.tensor(encode(text))
+print(data.shape, data.dtype)
+print(data[:1000])
+# this is the first 1000 characters from above in the character level tokens
