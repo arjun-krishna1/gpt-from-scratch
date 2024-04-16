716f84cafc33c705a6dba4ffa27c20d1b288774f
Arjun Krishna <arjunkrishna1306@gmail.com>
2024-04-16 09:44:13 -0400

    get dataset and load

---
+++ gpt-dev.py
@@ -0,0 +1,6 @@
+# See Karpathy's shared google colab jupyter notebook
+
+with open('input.txt', 'r', encoding='utf-8') as f:
+    text = f.read()
+
+print("length of dataset in characters: ", len(text))
